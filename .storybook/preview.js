16be8c01fd1f335611952718d86bb04bc9674ff7
Will McDonald <wemcdonald@gmail.com>
2023-09-05 16:20:20 -0700

    Update Switch theme (#258)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,12 +5,15 @@ import { ThemeProvider as EmotionThemeProvider } from "emotion-theming";
 import { BrowserRouter } from "react-router-dom";
 
 import SnackbarProvider from "../src/design-system/components/snackbar-provider";
-import ThemeProvider, { theme } from "../src/design-system/theme";
+import ThemeProvider, { darkTheme, theme as lightTheme } from "../src/design-system/theme";
+
+// toggle here to switch between light and dark theme
+const defaultTheme = "light";
 
 export const decorators = [
   Story => (
-    <ThemeProvider>
-      <EmotionThemeProvider theme={theme}>
+    <ThemeProvider colorScheme={defaultTheme}>
+      <EmotionThemeProvider theme={defaultTheme === "dark" ? darkTheme : lightTheme}>
         <BrowserRouter>
           <SnackbarProvider>{Story()}</SnackbarProvider>
         </BrowserRouter>
--- src/design-system/components/switch/index.tsx
+++ src/design-system/components/switch/index.tsx
@@ -1,19 +1,47 @@
-import { ChangeEvent } from "react";
+import React, { ChangeEvent } from "react";
 
 import MuiSwitch, { SwitchProps as MuiSwitchProps } from "@mui/material/Switch";
+import { useTheme } from "@mui/material/styles";
 
 import { InfoIconWithHelpTooltip, Stack, Typography } from "..";
 
-export type SwitchProps = Pick<MuiSwitchProps, "checked" | "disabled"> & {
-  label: string;
+type ColorProp = string | ((theme: any) => string);
+
+export type SwitchProps = Pick<MuiSwitchProps, "onChange" | "checked" | "disabled" | "icon" | "checkedIcon"> & {
+  label?: string;
   tooltip?: string;
-  onChange: (value: boolean) => void;
+  thumbColor?: ColorProp;
+  trackColor?: ColorProp;
+  trackColorChecked?: ColorProp;
+  dataCy?: string;
+  dataTestId?: string;
 };
 
-function Switch({ label, tooltip, checked, onChange, disabled }: SwitchProps) {
+function Switch({
+  onChange,
+  checked,
+  disabled,
+  icon,
+  checkedIcon,
+  label,
+  tooltip,
+  thumbColor,
+  trackColor,
+  trackColorChecked,
+  dataCy,
+  dataTestId,
+}: SwitchProps) {
+  const theme: any = useTheme();
+  const isDark = theme.palette.mode === "dark";
+
   const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    onChange(event.target.checked);
+    onChange?.(event, event.target.checked);
   };
+
+  if (trackColor && !trackColorChecked) {
+    trackColorChecked = trackColor;
+  }
+
   return (
     <Stack direction={"row"} spacing={0.5} alignItems={"center"}>
       <Typography
@@ -31,45 +59,56 @@ function Switch({ label, tooltip, checked, onChange, disabled }: SwitchProps) {
         checked={checked}
         onChange={onChangeHandler}
         disabled={disabled}
+        data-cy={dataCy}
+        data-testid={dataTestId}
         sx={{
           "width": 36,
           "height": 20,
           "padding": 0,
-          "& .MuiSwitch-switchBase": {
-            "padding": 0,
-            "transform": "scale(0.8)",
+          ".MuiSwitch-root": {
+            padding: "0",
+            height: "fit-content",
+            width: "fit-content",
+          },
+          ".MuiSwitch-thumb": {
+            height: "16px",
+            width: "16px",
+            boxShadow: "none",
+            color: thumbColor ?? "rgba(255, 255, 255, 1)",
+          },
+          ".MuiSwitch-track": {
+            height: "20px",
+            borderRadius: "20px",
+            width: "36px",
+            opacity: 1,
+            backgroundColor:
+              trackColor ?? ((theme: any) => (isDark ? "rgba(255, 255, 255, 0.4)" : theme.palette.secondary[40])),
+          },
+          ".MuiSwitch-switchBase": {
+            "padding": "2px",
+
             "&.Mui-checked": {
-              "transform": "translateX(16px)",
-              "& + .MuiSwitch-track": {
-                backgroundColor: theme => theme.palette.primary[20],
-              },
-              "&.Mui-disabled + .MuiSwitch-track": {
-                opacity: 0.5,
-              },
-              "& .MuiSwitch-thumb": {
-                background: theme => theme.palette.primary.gradient,
-              },
+              transform: "translateX(16px)",
             },
-            "&.Mui-disabled .MuiSwitch-thumb": {
-              background: "#BCC0C4",
-            },
-            "&.Mui-disabled + .MuiSwitch-track": {
-              backgroundColor: "rgba(0, 0, 0, 0.1)",
+            "&.Mui-checked + .MuiSwitch-track": {
+              backgroundColor: trackColorChecked ?? ((theme: any) => theme.palette.primary.gradient),
               opacity: 1,
             },
-          },
-          "& .MuiSwitch-thumb": {
-            backgroundColor: "#65676B",
-            boxShadow: "none",
-            width: 20,
-            height: 20,
-          },
-          "& .MuiSwitch-track": {
-            borderRadius: "12px",
-            backgroundColor: "rgba(0, 0, 0, 0.1)",
-            opacity: 1,
+            "&.Mui-disabled.Mui-checked + .MuiSwitch-track": {
+              backgroundColor: (theme: any) => (isDark ? "rgba(255, 255, 255, 0.3)" : theme.palette.secondary[30]),
+              opacity: 1,
+            },
+            "&.Mui-disabled + .MuiSwitch-track": {
+              backgroundColor: (theme: any) => (isDark ? "rgba(255, 255, 255, 0.3)" : theme.palette.secondary[30]),
+              opacity: 1,
+            },
+            "&.Mui-disabled > .MuiSwitch-thumb": {
+              opacity: 0.4,
+            },
           },
         }}
+        {...(icon && { icon })}
+        {...(checkedIcon && { checkedIcon })}
       />
     </Stack>
   );
